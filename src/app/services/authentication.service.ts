0d44a77a548b2e281355a7d57e9f5e1c8b4441db
Velmurugan Arumugam <vel.live@live.com>
2021-03-21 13:11:32 +0000

    Strict null check false

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -11,7 +11,7 @@ export class AuthenticationService {
     public currentUser: Observable<User>;
 
     constructor(private http: HttpClient) {
-        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')||'{}'));
         this.currentUser = this.currentUserSubject.asObservable();
     }
 
